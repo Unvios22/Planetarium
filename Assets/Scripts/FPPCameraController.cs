7dec49ba0dd316d0482006771bb3379130a04db9
Kajetan Jóźwiak <unvios22@gmail.com>
2019-05-17 17:52:45 +0200

    Add FPPCameraController

---
+++ Assets/Scripts/FPPCameraController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using ReadOnlyData;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+[RequireComponent(typeof(Camera))]
+public class FPPCameraController : MonoBehaviour {
+
+	[SerializeField] private float mouseSensitivity;
+	[SerializeField] private float maxY = 80f;
+	[SerializeField] private float minY = -80f;
+
+	[SerializeField] private float maxX = 360f;
+	[SerializeField] private float minX = 0f;
+	
+	[SerializeField] private bool invertYRot;
+	
+	[SerializeField] private float xRot;
+	[SerializeField] private float yRot;
+
+	private Camera _camera;
+	private Transform _cameraTransform;
+
+	private void Start() {
+		_camera = gameObject.GetComponent<Camera>();
+		_cameraTransform = transform;
+	}
+
+	private void LateUpdate() {
+		var xInputRot = Input.GetAxisRaw(InputStrings.Axis.MouseX) * mouseSensitivity;
+		var yInputRot = Input.GetAxisRaw(InputStrings.Axis.MouseY) * mouseSensitivity;
+
+		yInputRot = invertYRot ? yInputRot : -yInputRot;
+		//accounting for inverted/not inverted Y axis control
+		
+		xRot += xInputRot;
+		yRot += yInputRot;
+
+		ApplyRotConstraints();
+		ApplyRotation();
+	}
+
+	private void ApplyRotConstraints() {
+		if (yRot > maxY) {
+			yRot = maxY;
+		} 
+		else if (yRot < minY) {
+			yRot = minY;
+		}
+		
+		//TODO: possible user input loss
+		//In theory: rot 355 + 10 degree input rot in frame = 365 rot -> rounded to 0 with 5 input degree loss
+		if (xRot > maxX) {
+			xRot = minX;
+			
+		} 
+		else if (xRot < minX) {
+			xRot = maxX;
+		}
+	}
+
+	private void ApplyRotation() {
+		_cameraTransform.rotation = Quaternion.Euler(yRot, xRot,0f);
+		//Y mouse rot = X camera rot
+		//X mouse rot = Y camera rot
+	}
+	
+	public void RotateTo(float xRot, float yRot) {
+		this.xRot = xRot;
+		this.yRot = yRot;
+	}
+}
---
+++ Assets/Scripts/ReadOnlyData/InputStrings.cs
@@ -0,0 +1,12 @@
+namespace ReadOnlyData {
+	public class InputStrings {
+		public class Axis {
+			public static readonly string MouseX = "Mouse X";
+			public static readonly string MouseY = "Mouse Y";
+		}
+
+		public class Button {
+			
+		}
+	}
+}
