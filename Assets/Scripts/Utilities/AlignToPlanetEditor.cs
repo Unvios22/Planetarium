7a4fa114946823d0d4994546d8ce0a8ec319ab03
Kajetan Jóźwiak <unvios22@gmail.com>
2019-09-12 13:57:29 +0200

    add AlignToPlanet utility script

---
+++ Assets/Scripts/Utilities/AlignToPlanetEditor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CustomEditor(typeof(AlignToPlanetUtil))]
+public class AlignToPlanetEditor : Editor {
+	public override void OnInspectorGUI() {
+		DrawDefaultInspector();
+		var alignScript = (AlignToPlanetUtil) target;
+		if (GUILayout.Button("Realign this to planet!")) {
+				alignScript.RealignToPlanet();
+		}
+	}
+}
